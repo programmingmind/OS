21fcabe49eee8819b65b68f4eb40be5c10294358
Donovan McKelvey <Donovan.J.McKelvey@gmail.com>
2014-08-27 22:41:44 -0700

    we need all of this for the toolchain to work

---
+++ osx-toolchain/lib/gcc/i686-pc-elf/4.6.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../../sources/gcc-4.6.2/configure --prefix=/Users/berkus/Hobby/Metta/toolchain/gcc --target=i686-pc-elf --program-prefix=i686-pc-elf- --with-gmp=/Users/berkus/Hobby/Metta/toolchain/gcc --with-mpfr=/Users/berkus/Hobby/Metta/toolchain/gcc --with-mpc=/Users/berkus/Hobby/Metta/toolchain/gcc --with-system-zlib --enable-stage1-checking --enable-languages=c,c++ --disable-nls --disable-shared --disable-multilib";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "pentiumpro" } };
